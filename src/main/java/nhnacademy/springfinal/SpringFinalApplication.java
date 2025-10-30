68f18c300d98adbb668112e5dc15916cddb0cb55
JJung0 <junghwan__@naver.com>
2025-10-30 10:53:46 +0900

    redis 의존성 수정

--- src/main/java/nhnacademy/springfinal/SpringFinalApplication.java
+++ src/main/java/nhnacademy/springfinal/SpringFinalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringFinalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFinalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFinalApplication.class, args);
+    }
 
 }
